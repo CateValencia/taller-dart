1c397b18ba05cdd26102221d2cea9945782c728d
CateValencia <caterine.valencia@unillanos.edu.co>
2021-04-10 17:40:14 -0500

    crear carpeta y declarar datos

---
+++ 2.tipos/tipos.dart
@@ -0,0 +1,16 @@
+void main() {
+  int edad = 52;
+  double estatura = 1.80;
+
+  //num edad2 = 52;
+  //num estatura2 = 1.80;
+
+  if (edad.isNegative) {
+    print("este numero es negativo");
+    print(edad.abs());
+  } else {
+    print(edad.abs());
+  }
+
+  //print(edad);
+}
