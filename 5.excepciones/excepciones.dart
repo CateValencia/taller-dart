d59269ea5e40dc42d379a7abf979ded2271dbffc
CateValencia <caterine.valencia@unillanos.edu.co>
2021-04-10 18:49:17 -0500

    crear excepciones

---
+++ 5.excepciones/excepciones.dart
@@ -0,0 +1,21 @@
+void main () {
+ try {
+   print(obtenerNombre("juan"));
+   //print(obtenerNombreDos("Caterine"));
+ } 
+ //on FormatException catch (e) {
+ //   print("Se produjo un error de tipo FormatException");
+ }
+  catch (e) {
+    print("Se produjo un error en la aplicación, comunicar con el administrador");
+  }  finally {
+     print("Esto es un finally");
+  }
+}
+
+ String obtenerNombre(String? nombre) {
+   if (nombre == "Juan") {
+      throw FormatException("El nombre no puede ser $nombre");
+   }
+    return 'Mi nombre es $nombre';
+ }
