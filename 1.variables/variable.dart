a9607e712cc0be2e0d8b97ac90ee87d7d9c2b0a8
CateValencia <caterine.valencia@unillanos.edu.co>
2021-04-10 12:01:04 -0500

    agregar ariable null-safety

--- 1.variables/variable.dart
+++ 1.variables/variable.dart
@@ -2,8 +2,10 @@ void main () {
   String nombre = "juan";
   int edad = 23;
   String telefono = "3208003232";
+  int? variableAceptaNull = null;
 
   var apellido;
   print(nombre);
   print(apellido);
+  print(variableAceptaNull);
 }
