f7004212e8226758adedfd8a7c447d65dfb08e2e
CateValencia <caterine.valencia@unillanos.edu.co>
2021-04-10 18:55:26 -0500

    update declaración excepción

--- 5.excepciones/excepciones.dart
+++ 5.excepciones/excepciones.dart
@@ -1,10 +1,10 @@
 void main () {
  try {
-   print(obtenerNombre("juan"));
-   //print(obtenerNombreDos("Caterine"));
- } 
- //on FormatException catch (e) {
- //   print("Se produjo un error de tipo FormatException");
+   //print(obtenerNombre("juan"));
+    print(obtenerNombreDos("Caterine"));
+  } 
+ on FormatException catch (e) {
+    print("Se produjo un error de tipo FormatException");
  }
   catch (e) {
     print("Se produjo un error en la aplicación, comunicar con el administrador");
@@ -18,4 +18,13 @@ void main () {
       throw FormatException("El nombre no puede ser $nombre");
    }
     return 'Mi nombre es $nombre';
- }
+  }
+  
+  String obtenerNombreDos(String? nombre) {
+ 
+   if (nombre == 'Caterine') {
+      throw ("El nombre no puede ser $nombre");
+   }
+    return "mi nombre es $nombre";
+  }
+ 
