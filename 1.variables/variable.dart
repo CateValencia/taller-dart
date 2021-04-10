03127d93c3652a9c4c47845fac640af8ae7aec47
CateValencia <caterine.valencia@unillanos.edu.co>
2021-04-10 12:31:21 -0500

    agregar variable final

--- 1.variables/variable.dart
+++ 1.variables/variable.dart
@@ -4,8 +4,11 @@ void main () {
   String telefono = "3208003232";
   int? variableAceptaNull = null;
 
+  final String segundoApellido = "Herrera";
+
   var apellido;
   print(nombre);
   print(apellido);
+  print(segundoApellido);
   print(variableAceptaNull);
 }
