6ab5529a064f97dc9268d7a14f6f42da45b05c51
CateValencia <caterine.valencia@unillanos.edu.co>
2021-04-11 13:16:06 -0500

    update correccion&anónima

--- 3.funciones/funciones.dart
+++ 3.funciones/funciones.dart
@@ -2,20 +2,26 @@ void main () {
   var resultado1 = obtenerNombre ("Caterine");
   var resultado2 = obtenerNombreDos ("Carlos");
   var resultado3 = obtenerNombreTres (nombre: "Jose", edad: 18);
+  var resultado4 = miFuncion("Dayra");
   
   print(resultado1);
   print(resultado2);
   print(resultado3);
+  print(resultado4);
 }
 
-  String obtenerNombre(String valor) {
-    return 'Mi nombre es $valor';
+  var miFuncion = (String valor) {
+    return "Anonima: mi nombre es $valor";
+  };
+
+  String obtenerNombre(String? valor, [int? estatura, String? apellido]) {
+    return 'obtenerNombre : Mi nombre es $valor mi estatura es $estatura';
 
   }
 
   String obtenerNombreDos(String valor) => 'Mi nombre es $valor';
 
-  String obtenerNombreTres({String nombre = 'Pablo', int? edad}) {
+  String obtenerNombreTres({String nombre = 'Pedro', int? edad}) {
     return 'Mi nombre es $nombre y mi edad es $edad';
   }
     
