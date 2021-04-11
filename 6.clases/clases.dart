789ade788e7f5d902600f5421cbd547ef2727fd1
CateValencia <caterine.valencia@unillanos.edu.co>
2021-04-10 19:03:49 -0500

    crear clase empleado&vehículo

--- 6.clases/clases.dart
+++ 6.clases/clases.dart
@@ -4,10 +4,28 @@
    int? edad;
    
    Persona({this.nombre, this.apellido, this.edad});
+   
+   obtenerNombre() => this.nombre;
+ }
+
+ class Empleado extends Persona{
+   String? puesto;
+      
+   Empleado(this.puesto) : super(nombre: "juan", apellido: "valencia", edad: 28);
+ }
+
+class Vehiculo{
+   int? puertas;
+   String? color;
+   
+   //Vehiculo();
  }
   
  void main () {
   //var persona = new Persona("german", "herrera", 55);
   var persona = Persona(nombre: "caterine", apellido: "valencia", edad: 55); 
   print(persona);
+   
+  var empleado = Empleado("cajero"); 
+  print(empleado.obtenerNombre()); 
  }
